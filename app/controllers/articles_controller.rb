896d75a0c0aa00fed04d8264fe8dc620f7703656
Victor Miranda <mirandaguedes.victor@gmail.com>
2021-12-06 19:10:17 -0300

    Alterações no Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,18 +2,18 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
 
   def index
-    @highlights = Article.order(created_at: :desc).first(3)
+    @highlights = Article.desc_order.first(3)
 
     current_page = (params[:page] || 1).to_i
     
     highlights_ids = @highlights.pluck(:id).join(',')
 
     if @highlights.empty?
-       @articles = Article.order(created_at: :desc)
+       @articles = Article.desc_order
                           .page(current_page).per(2)
     else
-        @articles = Article.order(created_at: :desc)
-                          .where("id NOT IN(#{highlights_ids})")
+        @articles = Article.without_highlights(highlights_ids)
+                          .desc_order
                           .page(current_page).per(2)
     end
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5}
     validates :body, presence: true, length: { minimum: 5}
-        
+
+    paginates_per 2
+    
+    scope :desc_order, -> {order(created_at: :desc) }
+    scope :without_highlights, -> (ids) { where("id NOT IN(#{ids})") }
 end
 
 
