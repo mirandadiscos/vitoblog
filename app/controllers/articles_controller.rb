4157c19c6a10d2bddf009a91d888c2eb9ff5c962
Victor Miranda <mirandaguedes.victor@gmail.com>
2021-12-13 19:05:31 -0300

    add paginable, url amigavel category.name

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,23 @@
 class ArticlesController < ApplicationController
+  include Paginable
+
   before_action :authenticate_user!, except: %i[index show]
   before_action :set_article, only: %i[show edit update destroy]
 
   def index
-    @highlights = Article.desc_order.first(3)
+    category = Category.find_by_name(params[:category]) if params[:category.present?]
 
-    current_page = (params[:page] || 1).to_i
+    @highlights = Article.filter_by_category(category)
+                         .desc_order.first(3)
     
     highlights_ids = @highlights.pluck(:id).join(',')
-
+    
     @articles = Article.without_highlights(highlights_ids)
+                      .filter_by_category(category)
                       .desc_order
                       .page(current_page).per(2)
+
+    @categories = Category.sorted
   end
 
   def show; end
