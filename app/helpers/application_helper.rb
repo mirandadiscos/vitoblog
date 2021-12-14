32573928db1415b3374af69591b8fb3ea85f87ad
Victor Miranda <mirandaguedes.victor@gmail.com>
2021-12-14 16:08:32 -0300

    Ajustes deploy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
     def render_if(condition, template, record)
         render template, record if condition
     end
+
+    def sub_masked_email(email)
+        email.gsub(/(?<=.{2}).*@.*(?=\S{2})/, '****@****')
+    end
+
+    def month_day_comma_year(datetime)
+        datetime.strftime('%B %e, %Y')
+    end
 end
