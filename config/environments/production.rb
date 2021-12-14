3d349986af39836a826e9f6b2d4d98ef23224830
Victor Miranda <mirandaguedes.victor@gmail.com>
2021-12-14 14:51:12 -0300

    Corrigindo host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   #email
-  config.action_mailer.default_url_options = { host: 'vitoblog.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'mirandadiscos.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
